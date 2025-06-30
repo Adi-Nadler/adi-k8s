2185114ea58dc53c0ba6a9b339ddc3598e5ce83b
Adi Nadler <adidi1k@gmail.com>
2025-06-30 09:17:15 +0300

    Improve deploy.sh: add resource existence checks

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,23 @@ NODE_COUNT=2
 VM_SIZE="Standard_B2s"
 
 # Create resource group (if not exists)
-az group create --name $RESOURCE_GROUP --location $LOCATION
+if az group show --name $RESOURCE_GROUP &> /dev/null; then
+  echo "Resource group '$RESOURCE_GROUP' already exists."
+else
+  az group create --name $RESOURCE_GROUP --location $LOCATION
+fi
 
 # Create AKS cluster
-az aks create \
-  --resource-group $RESOURCE_GROUP \
-  --name $CLUSTER_NAME \
-  --node-count $NODE_COUNT \
-  --node-vm-size $VM_SIZE \
-  --generate-ssh-keys
+if az aks show --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME &> /dev/null; then
+  echo "AKS cluster '$CLUSTER_NAME' already exists."
+else
+  az aks create \
+    --resource-group $RESOURCE_GROUP \
+    --name $CLUSTER_NAME \
+    --node-count $NODE_COUNT \
+    --node-vm-size $VM_SIZE \
+    --generate-ssh-keys
+fi
 
 # Get kubeconfig
 az aks get-credentials -g $RESOURCE_GROUP -n $CLUSTER_NAME
